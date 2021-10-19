ad30e083601ca07feada8af6bbd47298b5033389
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-19 15:43:57 +0700

    Learn Lyfecycle with Annotation
    - @PostConstruct and @PreDestroy
    - must add javax.annotation-api-1.3.2.jar

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype") // @preDestroy prototype not call
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -26,6 +29,18 @@ public class TennisCoach implements Coach {
 //	public void setFortuneSErvice(FortuneService theFortuneService) {
 //		fortuneService = theFortuneService;
 //	}
+	
+	// java 9 up not support must add javax.annotation-api-1.3.2.jar
+	@PostConstruct 
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+	}
+	
+	// java 9 up not support must add javax.annotation-api-1.3.2.jar
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
+	}
 	
 	@Override
 	public String getDailyWorkout() {
