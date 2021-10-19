272a69ccef6563b796c85ba6234b9b95807ef2bf
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-19 15:08:35 +0700

    Learn Injection with Annotations
    - Constructor
    - Setter
    - Field
    - use whatever you need

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -12,6 +12,8 @@ public class AnnotationDemoApp {
 		
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
 		context.close();
 	}
 
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,36 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
+	private FortuneService fortuneService;
+
+//	@Autowired
+//	public TennisCoach(FortuneService fortuneService) {
+//		this.fortuneService = fortuneService;
+//	}
+	
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+//	@Autowired
+//	public void setFortuneSErvice(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
